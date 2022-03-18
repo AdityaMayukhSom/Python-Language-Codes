c6073ada4766ebfd848dc82139070de451fe67a1
Aditya Mayukh Som <adi.kg2@gmail.com>
2022-03-18 19:48:55 +0530

    Python Complete

--- Django/bookstore/books/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def index(request):
-    context = {'name': 'Aditya Mayukh Som'}
-    return render(request, './books/profile.html', context)
